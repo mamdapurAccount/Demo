88ba9073956ad436ae7afa201711b4266a9bb950
MK <manatharv@gmail.com>
2022-12-05 20:41:39 +0530

    loo

---
+++ src/test/java/Mytest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+public class Mytest {
+
+	@Test
+	public void test() {
+		assertEquals("Hello", "Hello");
+		
+	}
+
+}
