367095362b9d20604fb544aaa5a0aaf32d01db01
mamdapurAccount <56784976+mamdapurAccount@users.noreply.github.com>
2025-04-29 06:55:01 +0530

    Add files via upload
    First Time

---
+++ SSOConsume/src/main/java/com/aditi/SsoConsumeApplication.java
@@ -0,0 +1,13 @@
+package com.aditi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoConsumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoConsumeApplication.class, args);
+	}
+
+}
---
+++ SSOConsume/src/test/java/com/aditi/SsoConsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoConsumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
